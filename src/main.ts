b4c32f821d5bf081a0e8fab5d3668f0975967ff9
Cameron Green <cameron@camerongreens.com>
2021-07-29 23:56:23 -0500

    vite init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Seed City</h1>
+  <h2>scaffolded with Vite!</h2>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
